ceaaaf9b727bd6a62b62aaeca7c6d7f707fd42cf
Timo Van-Kissing <timo.van-kissing@appfolio.com>
2020-06-16 19:20:03 -0700

    fix: remove stores dependency from <App />

--- app/javascript/image_sharing/components/test/App.spec.js
+++ app/javascript/image_sharing/components/test/App.spec.js
@@ -3,18 +3,12 @@ import { shallow } from 'enzyme';
 import { describe, it } from 'mocha';
 import React from 'react';
 
-import { FeedbackStore } from '../../stores/FeedbackStore';
 import App from '../App';
 import Footer from '../Footer';
 import Header from '../Header';
 
-
-const stores = {
-  feedbackStore: new FeedbackStore()
-};
-
 describe('<App />', () => {
-  const wrapper = shallow(<App stores={stores} />).dive();
+  const wrapper = shallow(<App />);
 
   it('should have a header and footer', () => {
     expect(wrapper.find(Footer)).to.be.lengthOf(1);
