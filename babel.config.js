77a4111f55425a1de35de05d470c6503b4fcf9ac
Timo Van-Kissing <timo.van-kissing@appfolio.com>
2020-09-22 00:06:00 -0700

    chore: upgrade dev dependencies

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,21 @@
 module.exports = {
-  presets: [[
-    '@babel/preset-env',
-    {
-      targets: {
-        node: '10',
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: '10',
+        }
       }
-    }
-  ], '@babel/preset-react'],
+    ],
+    '@babel/preset-react',
+    // ['@babel/preset-typescript', { 'allExtensions': true, 'isTSX': true }]
+  ],
+
   plugins: [
     ['@babel/plugin-proposal-decorators', { legacy: true }],
     ['@babel/plugin-proposal-class-properties', { loose: true }],
-    '@babel/plugin-transform-object-assign'
+    '@babel/plugin-transform-object-assign',
+    '@babel/plugin-transform-modules-commonjs'
   ]
 };
