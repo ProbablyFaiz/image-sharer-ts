c92019d6b78db37be27bfc50691a50a12601a960
Timo Van-Kissing <timo.van-kissing@appfolio.com>
2020-09-22 00:06:00 -0700

    chore: upgrade dev dependencies

---
+++ .mocharc.js
@@ -0,0 +1,26 @@
+'use strict';
+
+// Here's a JavaScript-based config file.
+// If you need conditional logic, you might want to use this type of config.
+// Otherwise, JSON or YAML is recommended.
+
+module.exports = {
+  diff: true,
+  exit: true,
+  extension: ['js', 'ts', 'tsx'],
+  file: './app/javascript/testHelpers.js',
+  package: './package.json',
+  r: [
+    'esm',
+    '@babel/register',
+    'ignore-styles',
+  ],
+  recursive: true,
+  reporter: 'spec',
+  reporterOption: [
+    'mochaFile=./test_reports/test-results.xml'
+  ],
+  slow: 75,
+  timeout: 2000,
+  ui: 'bdd'
+};
--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,20 @@
 module.exports = {
-  presets: [[
-    '@babel/preset-env',
-    {
-      targets: {
-        node: '10',
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: '10',
+        }
       }
-    }
-  ], '@babel/preset-react'],
+    ],
+    '@babel/preset-react',
+  ],
+
   plugins: [
     ['@babel/plugin-proposal-decorators', { legacy: true }],
     ['@babel/plugin-proposal-class-properties', { loose: true }],
-    '@babel/plugin-transform-object-assign'
+    '@babel/plugin-transform-object-assign',
+    '@babel/plugin-transform-modules-commonjs'
   ]
 };
