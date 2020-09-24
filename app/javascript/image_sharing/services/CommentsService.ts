3f5b1b65514c4f51886359ec404d6bd0fce58cf3
Faiz Surani <faiz.surani@gmail.com>
2020-09-24 10:00:08 -0700

    feat: add create comment method to CommentsService

--- app/javascript/image_sharing/services/CommentsService.ts
+++ app/javascript/image_sharing/services/CommentsService.ts
@@ -1,4 +1,4 @@
-import { get } from '../utils/helper';
+import {get, post} from '../utils/helper';
 
 export interface Comment {
     created_at: string;
@@ -11,6 +11,10 @@ export type CommentsResponse = Comment[];
 export class CommentsService {
     static getComments(image_id: string) {
         return get(`/api/comments?image_id=${image_id}`) as Promise<CommentsResponse>;
+    }
+
+    static createComment(image_id: string, text: string) {
+      return post('/api/comments', { image_id, text });
     }
 }
 
