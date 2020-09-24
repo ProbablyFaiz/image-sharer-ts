0cb941cc0b38ee5d02a7653d8eb20db0d9d57c43
Faiz Surani <faiz.surani@gmail.com>
2020-09-24 10:02:11 -0700

    feat: add comment submission to CommentsStore

--- app/javascript/image_sharing/stores/CommentsStore.ts
+++ app/javascript/image_sharing/stores/CommentsStore.ts
@@ -14,6 +14,16 @@ export class CommentsStore {
       this.comments = comments;
     });
   }
+
+  @action submitComment = async (commentText: string) => {
+    await CommentsService.createComment(this._imageId, commentText);
+    const newComment: Comment = {
+      id: (this.comments[this.comments.length - 1]?.id ?? 0) + 1,
+      text: commentText,
+      created_at: 'just now'
+    }
+    this.comments.push(newComment);
+  }
 }
 
 export default CommentsStore;
