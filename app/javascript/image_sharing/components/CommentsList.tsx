e7c31eb1af6fc60d45eccf31487e754124577c64
Timo Van-Kissing <timo.van-kissing@appfolio.com>
2020-09-24 10:46:23 -0700

    wip: commentlist can render comment now

--- app/javascript/image_sharing/components/CommentsList.tsx
+++ app/javascript/image_sharing/components/CommentsList.tsx
@@ -1,12 +1,31 @@
-import {observer} from "mobx-react";
-import React, {Component, Fragment} from 'react';
+import React, { Component, Fragment } from 'react';
+import { observer } from "mobx-react";
+
+interface Comment {
+  created_at: string;
+  id: number;
+  text: string;
+}
+
+interface CommentsListProps {
+  comments?: Comment[];
+}
 
 @observer
-class CommentsList extends Component {
+class CommentsList extends Component<CommentsListProps> {
   render() {
     return (
       <Fragment>
         <h2>Comments</h2>
+        {this.props.comments?.map(comment =>
+          <div key={comment.id}>
+            {comment.text}
+            <div style={{ color: "grey", fontSize: "14px" }}>
+              {comment.created_at}
+            </div>
+            <br />
+          </div>
+        )}
       </Fragment>
     );
   }
